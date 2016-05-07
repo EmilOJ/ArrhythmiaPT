d07079e1823a60660c988bc55f83515a38881904
EØJ <emil.johansen@gmail.com>
2016-05-07 10:14:22 +0200

    smårettelser

--- app/src/main/java/com/helge/arrhythmiapt/SignalProcessing.java
+++ app/src/main/java/com/helge/arrhythmiapt/SignalProcessing.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.helge.arrhythmiapt.Models.SVMStruct;
 
 import java.io.BufferedReader;
@@ -23,22 +22,67 @@ import jwave.transforms.wavelets.biorthogonal.BiOrthogonal35;
 public class SignalProcessing {
 
     static final String TAG = "SignalProcessing";
-    final Context mContext;
     static final int NUMBER_OF_FEATURES = 17;
     static final int FS = 360; // Sample rate in Hz
-    static final int SEGMENT_LENGTH = (int) Math.floor((200.0 / 1000) * FS); // ms (on each side of QRS complex) //burg math.floor
-    static final int REFRACTORY_PERIOD = (int) Math.floor((250.0 / 1000) * FS); // ms
-    // Instance variable containing ECG mSignal
-    public List<Double> mSignal = new ArrayList<Double>();
-    public ArrayList<ArrayList<Double>> mSegments = new ArrayList<ArrayList<Double>>();
-    public List<Integer> mQrs;
+    static final int SEGMENT_LENGTH = (int) Math.floor((200.0 / 1000) * FS);
+    static final int REFRACTORY_PERIOD = (int) Math.floor((250.0 / 1000) * FS);
     private static SVMStruct mSVMStruct;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+    final Context mContext;
+    public List<Double> mSignal = new ArrayList<Double>();
+    public ArrayList<ArrayList<Double>> mSegments = new ArrayList<>();
+    public List<Integer> mQrs;
 
+    public SignalProcessing(Context context) {
+        mContext = context;
+        mSVMStruct = new SVMStruct(mContext);
+
+    }
+
+    // has to be sorted before - see MD app Fragment
+    public static double median(double[] m) {
+        int middle = m.length / 2;
+        if (m.length % 2 == 1) {
+            return m[middle];
+        } else {
+            return (m[middle - 1] + m[middle]) / 2.0;
+        }
+    }
+
+    // http://stackoverflow.com/questions/4191687/how-to-calculate-mean-median-mode-and-range-from-a-set-of-numbers :
+    // http://stackoverflow.com/questions/8835464/qrs-detection-in-java-from-ecg-byte-array
+    public static double mean(int[] m) {
+        double sum = 0;
+        for (int i = 0; i < m.length; i++) {
+            sum += m[i];
+        }
+        return sum / m.length;
+    }
+
+    public static double mean(List<Double> m) {
+        double sum = 0;
+        for (int i = 0; i < m.size(); i++) {
+            sum += m.get(i);
+        }
+        return sum / m.size();
+    }
+    // Final function output
+
+
+    //public int[] circshift(int[] last_qrs) {
+    //    for (int i = 0; i < last_qrs.length-1; i++) {
+    //        last_qrs[4-i] = last_qrs[3-i];
+    //    }
+    //    return last_qrs;
+    //}
+
+    public static List<Double> demean(List<Double> m) {
+        double mMean = mean(m);
+        List<Double> dSignal = new ArrayList<>();
+        for (int i = 0; i < m.size(); i++) {
+            dSignal.add(m.get(i) - mMean);
+        }
+        return dSignal;
+    }
 
     public void readECG() throws IOException {
 
@@ -71,64 +115,40 @@ public class SignalProcessing {
         }
     }
 
-    public SignalProcessing(Context context) {
-        mContext = context;
-        mSVMStruct = new SVMStruct(mContext);
-
-    }
-
     public void detect_and_classify() {
-        List<Integer> qrsArray = new ArrayList<Integer>();
-        ArrayList<ArrayList<Double>> segments; // = new ArrayList<ArrayList<Double>>();
+        List<Integer> qrs_detected;
+        ArrayList<ArrayList<Double>> segments;
         double[] features = new double[NUMBER_OF_FEATURES];
         int detected_qrs;
         int classification;
 
+        qrs_detected = detect_qrs();
 
-        // only run QRS detection algorithm if mSignal is longer than refractory period
-        if (mSignal.size() > REFRACTORY_PERIOD) {
+        // Extract ±200 ms mSegments around QRS. This is used for classification.
+        segments = segments_around_qrs(qrs_detected);
 
-            // Location (array index) of detected QRS. 0 if no QRS detected.
-            List<Integer> qrs_detected = detect_qrs();
+        // Compute features
+        features = get_features(segments, qrs_detected);
 
-            if (qrs_detected.size() != 0) {
-                // Add detected QRS location to previous ones (need 3 for classification)
+        // Classify with support vector machine
+        /*classification = classify_segments(segments, features);*/
 
-                //mQrs.add(qrs_detected);
-                //qrsArray.add(qrs_detected);
+        // Save classification and mSignal to database
+        /*save_classification(classification);*/
 
-                qrsArray = qrs_detected;
+        // TODO: variable 'mSignal' must be emptied like below, so a new QRS detection can be performed,
+        // but the mSignal must also be saved to a variable with continuous ECG mSignal containing
+        // all three QRS complexes can be accessed. This is needed for RR interval computation.
 
-
-
-
-                // Extract ±200 ms mSegments around QRS. This is used for classification.
-                segments = segments_around_qrs(qrsArray);
-
-                // Compute features
-                features = get_features(segments, qrsArray);
-
-                // Classify with support vector machine
-                /*classification = classify_segments(segments, features);*/
-
-                // Save classification and mSignal to database
-                /*save_classification(classification);*/
-
-                // TODO: variable 'mSignal' must be emptied like below, so a new QRS detection can be performed,
-                // but the mSignal must also be saved to a variable with continuous ECG mSignal containing
-                // all three QRS complexes can be accessed. This is needed for RR interval computation.
-
-                // Empty mSignal variable
-                //this.mSignal = new ArrayList<Double>();
+        // Empty mSignal variable
+        //this.mSignal = new ArrayList<Double>();
 
 
-                //}
-            }
-        }
+        //}
+
     }
 
     private List<Integer> detect_qrs() {
-
 
 
 /**
@@ -176,7 +196,7 @@ public class SignalProcessing {
 
         //Keeping track of maximum values in last 5 windows
         double[] window_max_buff = new double[5];
-        double window_max =0;
+        double window_max = 0;
         int[] last_qrs = new int[5];
 
         int time_since_last_qrs;
@@ -184,7 +204,7 @@ public class SignalProcessing {
 
         double rr_cur = 0;
         double rr_last = 0;
-        int[] rr = new int[last_qrs.length-1];
+        int[] rr = new int[last_qrs.length - 1];
 
         //// Filter Stage
         List<Double> b_low = new ArrayList<>(Arrays.asList(-0.00300068847555824, -0.0888956549729993, -0.00978073251699008, -0.00913537555132255, -0.00348493467952550, 0.00341086079804900, 0.0102865391156571, 0.0156935263250855, 0.0182699445494703, 0.0171711581672353, 0.0120921576301221, 0.00357779560317851, -0.00713041168615247, -0.0180134156879530, -0.0268696487375390, -0.0313147178636851, -0.0295416693658266, -0.0203155162205885, -0.00356416221151197, 0.0196106546763700, 0.0473078208714944, 0.0765698351683510, 0.104067354017840, 0.126566433194386, 0.141348431488521, 0.146497228887632, 0.141348431488521, 0.126566433194386, 0.104067354017840, 0.0765698351683510, 0.0473078208714944, 0.0196106546763700, -0.00356416221151197, -0.0203155162205885, -0.0295416693658266, -0.0313147178636851, -0.0268696487375390, -0.0180134156879530, -0.00713041168615247, 0.00357779560317851, 0.0120921576301221, 0.0171711581672353, 0.0182699445494703, 0.0156935263250855, 0.0102865391156571, 0.00341086079804900, -0.00348493467952550, -0.00913537555132255, -0.00978073251699008, -0.0888956549729993, -0.00300068847555824));
@@ -192,10 +212,10 @@ public class SignalProcessing {
         List<Double> b_avg = new ArrayList<>(Arrays.asList(0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625));
 
         // Lowpass Filter
-        mSignal = filter(mSignal,b_low);
+        mSignal = filter(mSignal, b_low);
 
         // Highpass Filter
-        mSignal = filter(mSignal,b_high);
+        mSignal = filter(mSignal, b_high);
 
         // Subtract mean
         mSignal = demean(mSignal);
@@ -204,7 +224,7 @@ public class SignalProcessing {
         mSignal = abs(mSignal);
 
         // Average
-        mSignal = filter(mSignal,b_avg);
+        mSignal = filter(mSignal, b_avg);
 
 
         /* Detection*/
@@ -221,7 +241,7 @@ public class SignalProcessing {
 
             // Check if refractory period is over
             // (If not, don't check for new QRS)
-            time_since_last_qrs =  i - last_qrs[0];
+            time_since_last_qrs = i - last_qrs[0];
 
             if ((time_since_last_qrs > REFRACTORY_PERIOD) || first_candidate) {
                 // Check if a candidate QRS was detected
@@ -273,7 +293,6 @@ public class SignalProcessing {
                             //(window_max_buff) weighted by threshold correction
                             // factor (thresh_correct)
                             h_thresh = h_thresh_correct * median(neglectZeros(window_max_buff));
-                            ;
                             // Reset window_max
                             window_max = 0;
 
@@ -324,15 +343,6 @@ public class SignalProcessing {
 
         //TODO: if qrs_loc contains no ones - return empty list
     }
-    // Final function output
-
-
-    //public int[] circshift(int[] last_qrs) {
-    //    for (int i = 0; i < last_qrs.length-1; i++) {
-    //        last_qrs[4-i] = last_qrs[3-i];
-    //    }
-    //    return last_qrs;
-    //}
 
     public int[] diff(int[] last_qrs) {
         int[] rr = new int[4];
@@ -341,6 +351,7 @@ public class SignalProcessing {
         }
         return rr;
     }
+
     public int[] neglectZeros(int[] rr) {
         int j = 0;
         for (int k = 0; k < rr.length; k++) {
@@ -364,55 +375,18 @@ public class SignalProcessing {
     }
 
     public int[] abs(int[] array) {
-        for (int i = 0; i < array.length;i++) {
-           array[i] = Math.abs(array[i]);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.abs(array[i]);
         }
         return array;
     }
 
     public List<Double> abs(List<Double> array) {
         List<Double> absArray = new ArrayList<>();
-        for (int i = 0; i < array.size();i++) {
+        for (int i = 0; i < array.size(); i++) {
             absArray.add(Math.abs(array.get(i)));
         }
         return absArray;
-    }
-
-    // has to be sorted before - see MD app Fragment
-    public static double median(double[] m) {
-        int middle = m.length/2;
-        if (m.length%2 == 1) {
-            return m[middle];
-        } else {
-            return (m[middle-1] + m[middle]) / 2.0;
-        }
-    }
-
-    // http://stackoverflow.com/questions/4191687/how-to-calculate-mean-median-mode-and-range-from-a-set-of-numbers :
-    // http://stackoverflow.com/questions/8835464/qrs-detection-in-java-from-ecg-byte-array
-    public static double mean(int[] m) {
-        double sum = 0;
-        for (int i = 0; i < m.length; i++) {
-            sum += m[i];
-        }
-        return sum / m.length;
-    }
-
-    public static double mean(List<Double> m) {
-        double sum = 0;
-        for (int i = 0; i < m.size(); i++) {
-            sum += m.get(i);
-        }
-        return sum / m.size();
-    }
-
-    public static List<Double> demean(List<Double> m) {
-        double mMean = mean(m);
-        List<Double> dSignal = new ArrayList<>();
-        for (int i = 0; i < m.size(); i++) {
-            dSignal.add(m.get(i) - mMean);
-        }
-        return dSignal;
     }
 
     public int[] circshift(int[] array, int shift){
@@ -458,7 +432,6 @@ public class SignalProcessing {
 
     private ArrayList<ArrayList<Double>> segments_around_qrs(List<Integer> qrsArray) {
         // INPUT:
-        //      - mQrs:      location in sample number of the detected mQrs complex
         //      - mSignal:   raw mSignal (or filtered mSignal from detect_qrs()?)
         // OUTPUT:
         //      - mSegments:  The three mSegments consisting of ±200 ms around each QRS complex
@@ -563,20 +536,6 @@ public class SignalProcessing {
         }
 
         return classification;
-    }
-
-    private double[] innerProduct(double[][] a, double[] b) {
-        double[] product = new double[a.length];
-        int sum;
-        for (int row = 0; row < a.length - 1; row++) {
-            sum = 0;
-            for (int col = 0; col < b.length - 1; col++) {
-                sum += a[row][col] * b[col];
-            }
-            product[row] = sum;
-        }
-
-        return product;
     }
 
     private double innerProduct(double[] a, double[] b) {
