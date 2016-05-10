45ea5dadd094353d60177878cccbf04f82df68a3
tschblom <tschblom@gmail.com>
2016-05-10 11:54:28 +0200

    K=300

--- app/src/main/java/com/helge/arrhythmiapt/MainMenu.java
+++ app/src/main/java/com/helge/arrhythmiapt/MainMenu.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.helge.arrhythmiapt.Notes.NotesListActivity;
 
@@ -17,6 +18,29 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        String patientName = "Lone Hansen";
+        String patientCPR = "";
+        // Name of patient
+        TextView textView6 = (TextView) findViewById(R.id.name);
+        textView6.setText(patientName);
+
+        // cpr-number
+        TextView textView7 = (TextView) findViewById(R.id.cpr);
+        textView7.setText(patientCPR);
+
+        // Medicin that patient takes
+        TextView textView8 = (TextView) findViewById(R.id.med1);
+        textView8.setText("ChlorTrimeton");
+        //textView8.setText(patientMedicin);
+        TextView textView9 = (TextView) findViewById(R.id.med2);
+        textView9.setText("(allergy and hay fever)");
+        TextView textView10 = (TextView) findViewById(R.id.med3);
+        textView10.setText("Ventolin");
+        //textView8.setText(patientMedicin);
+        TextView textView11 = (TextView) findViewById(R.id.med4);
+        textView11.setText("(asthma and obstructive pulmonary disease)");
+
 
         notesButton = (Button) findViewById(R.id.notesButton);
 
